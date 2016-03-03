ecd159efd24fe1fdfa774bee5ef597493b02d38a
Betty <bettsy716@gmail.com>
2016-03-03 16:11:54 -0600

    saludo
    saludo respecto a porcentajes usando if

---
+++ Ejercicio if.R
@@ -0,0 +1,11 @@
+x <- 36
+if ( 1<x & x<10){
+    y <- "que pedo we" 
+}else if (11<x & x<30) {
+    y <- "que onda"
+}else if(31<x & x<60){
+    y <- "que pex"
+}else if (81<x & x<100){
+    y <- "quiubo"
+}
+y
